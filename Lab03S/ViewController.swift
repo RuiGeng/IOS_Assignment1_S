bb3fd827121fabc49376c007b6c3b5817a6e7097
Rui Geng <gengr.r+ca@gmail.com>
2016-09-22 16:28:09 -0400

    Initial Commit

---
+++ Lab03S/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab03S
+//
+//  Created by Rui Geng on 2016-09-22.
+//  Copyright © 2016 Rui Geng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
