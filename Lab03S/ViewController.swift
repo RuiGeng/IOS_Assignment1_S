f6c1c0f1f65ca2c0e3804db4765424dd55760970
Rui Geng <ruigeng@Students-MacBook-Pro.local>
2016-09-27 10:39:50 -0400

    first commit

--- Lab03S/ViewController.swift
+++ Lab03S/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabSalary: UILabel!
+    
+    @IBOutlet weak var SliderSalary: UISlider!
+    
+    @IBOutlet weak var LabSecondSalary: UILabel!
+    
+    private var currentSalary:Int = 0
+    
+    private var timer = NSTimer()
+    
+    private let DAY:Double = 365
+    
+    private let HOUR:Double = 24
+    
+    private let MINUTES:Double = 60
+    
+    private let SECOND:Double = 60
+    
+    private var secondSalary:Double = 0
+    
+    private var updateSalary:Double = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentSalary = Int(SliderSalary.value)
+        
+        LabSalary.text = "Annual Salary: $ \(currentSalary)"
+        
+        updateSalary = 0
+        
+        secondSalary = Double(currentSalary)/DAY/HOUR/MINUTES/SECOND
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTimer(sender: UIButton) {
+        
+        if(!timer.valid){
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+        }
+        
+
+    }
+    
+    @IBAction func stopTimer(sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        currentSalary = Int(sender.value)
+        LabSalary.text = "Annual Salary: $ \(currentSalary)"
+        secondSalary = Double(currentSalary)/DAY/HOUR/MINUTES/SECOND
+    }
+    
+    func updateCounter() {
+        
+        updateSalary = updateSalary + secondSalary
+        
+        LabSecondSalary.text = String.localizedStringWithFormat("$ %0.2f", updateSalary)
+    }
 
 }
 
